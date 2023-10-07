1712ff01dd7e7ad3cb54404bf85eea7ac067c4ca
TomChan <chanshingka1@gmail.com>
2023-10-08 07:36:11 +0800

    Add the Aladin plugin

---
+++ src/components/Aladin.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Aladin = ({target}) => {
+
+    React.useEffect(() => {
+        let aladin = A.aladin('#aladin-lite-div', { survey: 'P/DSS2/color', fov:0.035, target: target })
+        aladin.setFov(1)
+    }, [])
+
+    return (
+        <div id='aladin-lite-div' style={{ width: '400px', height: '400px' }} />
+    )
+}
+
+export default Aladin
